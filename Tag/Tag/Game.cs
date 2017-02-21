b89fe08e0175a5532615fbaeb43e86d17312f6d1
nastya-fedina <nastya-fedina96@ya.ru>
2017-02-21 21:04:10 +0500

    Метод shift 001

--- Tag/Tag/Game.cs
+++ Tag/Tag/Game.cs
@@ -20,5 +20,98 @@ namespace Tag
                     GameBoard[i].Add(0);
         }
 
+        public int Indexer(int x, int y)
+        {
+            if (x >= Counter || y >= Counter || x < 0 || y < 0)
+            {
+                {
+                    throw new ArgumentException();
+                }
+            }
+            else return GameBoard[x][y];
+        }
+
+        public void Shift(int value)
+        {
+            int x = 0, y = 0;
+            for (int i = 0; i < Counter; i++)
+            {
+                bool flag = true;
+                for (int j = 0; j < Counter; j++)
+                    if (GameBoard[i][j] == value)
+                    {
+                        x = i; y = j;
+                        flag = false;
+                        break;
+                    }
+                if (!flag) break;
+            }
+            if (x < Counter - 1 && y < Counter - 1)
+            {
+                if (GameBoard[x + 1][y] == 0)
+                {
+                    GameBoard[x + 1][y] = value;
+                    GameBoard[x][y] = 0;
+                    return;
+                }
+                else if (GameBoard[x][y + 1] == 0)
+                {
+                    GameBoard[x][y + 1] = value;
+                    GameBoard[x][y] = 0;
+                    return;
+                }
+                else if (GameBoard[x + 1][y + 1] == 0)
+                {
+                    GameBoard[x + 1][y + 1] = value;
+                    GameBoard[x][y] = 0;
+                    return;
+                }
+            }
+
+            else if (x > 0 && y > 0)
+            {
+                if (GameBoard[x - 1][y] == 0)
+                {
+                    GameBoard[x - 1][y] = value;
+                    GameBoard[x][y] = 0;
+                    return;
+                }
+                else if (GameBoard[x][y - 1] == 0)
+                {
+                    GameBoard[x][y - 1] = value;
+                    GameBoard[x][y] = 0;
+                    return;
+                }
+                else if (GameBoard[x - 1][y - 1] == 0)
+                {
+                    GameBoard[x - 1][y - 1] = value;
+                    GameBoard[x][y] = 0;
+                    return;
+                }
+            }
+            else if (x > 0 && y < Counter -1)
+            {
+                if (GameBoard[x - 1][y + 1] == 0)
+                {
+                    GameBoard[x - 1][y + 1] = value;
+                    GameBoard[x][y] = 0;
+                    return;
+                }
+            }
+            else if (x < Counter - 1 && y > 0)
+            {
+                if (GameBoard[x + 1][y - 1] == 0)
+                {
+                    GameBoard[x + 1][y - 1] = value;
+                    GameBoard[x][y] = 0;
+                    return;
+                }
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+
     }
 }
