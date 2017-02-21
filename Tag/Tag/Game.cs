70b6a9cb50bbe180e3c4f47b1e22bfd763b51cbd
nastya-fedina <nastya-fedina96@ya.ru>
2017-02-21 18:45:15 +0500

    001

---
+++ Tag/Tag/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tag
+{
+    class Game
+    {
+        public readonly int Counter;
+        public List<List<int>> GameBoard;
+        public Game(int count)
+        {
+            Counter = count;
+            List<List<int>> GameBoard = new List<List<int>>();
+
+            for (int i = 0; i < Counter; i++)
+                for (int j = 0; j < Counter; j++)
+                    GameBoard[i].Add(0);
+        }
+
+    }
+}
---
+++ Tag/Tag/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tag
+{
+    class Program
+    {
+        static int Main()
+        {
+            int counter = 0;
+            Console.WriteLine("Input size of the game");
+
+            try
+            {
+                counter = int.Parse(Console.ReadLine());
+                if (counter < 0) throw new ArgumentException();
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("It's not a positive number\n");
+                return Main();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("It's not a number\n");
+                return Main();
+            }
+
+
+
+            return 0;      
+        }
+    }
+}
