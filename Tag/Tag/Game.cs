c205d7a3b0268349c9f5a0e4c5090b392bafd21c
nastya-fedina <nastya-fedina96@ya.ru>
2017-02-24 17:29:14 +0500

    Конструктор
    Создание и заполнение игрового поля переданными значениями, заполнение
    таблицы координат значений. + Исключения (без обработки) на некорректные
    переданные числа

--- Tag/Tag/Game.cs
+++ Tag/Tag/Game.cs
@@ -8,16 +8,59 @@ namespace Tag
 {
     class Game
     {
-        public readonly int Counter;
+        public readonly int Counter; //кол-во ячеек
+        public readonly int BoardSize;
         public List<List<int>> GameBoard;
-        public Game(int count)
+        public int[][] ValueLocation; 
+        public Game(params int[] value)
         {
-            Counter = count;
-            List<List<int>> GameBoard = new List<List<int>>();
+            int counter = 0;
+            foreach (int val in value)
+                counter++;
 
+            if (Math.Sqrt(counter) % 1 != 0)
+            {
+                throw new ArgumentException();
+            }
+
+            this.Counter = counter;
+            this.BoardSize = (int)Math.Sqrt(counter);
+
+            List<List<int>> GameBoard = new List<List<int>>();
+            for (int i = 0; i < BoardSize; i++)
+                for (int j = 0; j < BoardSize; j++)
+                    GameBoard[i].Add(-1);
+
+            int [][] ValueLocation = new int[Counter][];
             for (int i = 0; i < Counter; i++)
-                for (int j = 0; j < Counter; j++)
-                    GameBoard[i].Add(0);
+            {
+                ValueLocation[i] = new int[2];
+                ValueLocation[i][0] = -1;
+                ValueLocation[i][1] = -1;
+            }
+
+
+            counter = 0;
+            for (int i = 0; i < BoardSize; i++)
+            {
+                for (int j = 0; j < BoardSize; j++)
+                {
+                    if (value[counter] > Counter - 1)
+                    {
+                        throw new IndexOutOfRangeException(); //недопустимое значение ячейки
+                    }
+
+                    if (ValueLocation[value[counter]][0] != -1 && ValueLocation[value[counter]][1] != -1)
+                    {
+                        throw new ArgumentException(); //повторяющееся значение ячейки
+                    }
+
+                    GameBoard[i][j] = value[counter];
+                    ValueLocation[value[counter]][0] = i;
+                    ValueLocation[value[counter]][1] = j;
+                    counter++;
+                }
+            }                   
         }
 
         public int Indexer(int x, int y)
